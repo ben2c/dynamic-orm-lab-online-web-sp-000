8e9a741b0c38aea7f6667b854c9ac5b309f29966
Benjamin Cheng <bencheng200@gmail.com>
2020-12-15 15:47:27 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
   def self.table_name
     self.to_s.downcase.pluralize
   end
@@ -21,4 +21,6 @@ class InteractiveRecord
 
     column_names.compact
   end
+
+
 end
