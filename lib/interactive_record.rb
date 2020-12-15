c35f3f70f6b87cbb1f3279ec354315de06016204
Benjamin Cheng <bencheng200@gmail.com>
2020-12-15 16:00:06 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -36,4 +36,9 @@ class InteractiveRecord
     self.class.table_name
   end
 
+  def col_names_for_insert
+    self.class.column_names.delete_if do |col|
+      col == "id".join(", ")
+    end
+  end
 end
